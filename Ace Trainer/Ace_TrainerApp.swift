3c573d995d869e125e35f2ecc50715c39baeb3ea
Tomohiro Sasaki <takatobi0326@icloud.com>
2022-07-10 16:53:58 +0900

    Initial Commit

---
+++ Ace Trainer/Ace_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ace_TrainerApp.swift
+//  Ace Trainer
+//
+//  Created by user on 2022/07/10.
+//
+
+import SwiftUI
+
+@main
+struct Ace_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
